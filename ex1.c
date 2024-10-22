8790e69d197f4e667fbb0d0bce477d6252d743c4
MuhammedElubeyid <eprrmkz12345@gmail.com>
2024-10-22 16:35:21 +0300

    I am sendex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void) {
+int num = get_int("guess a number between 1 and 5");
+  if (num>=0 || num<= 5){
+   printf("you got it right");
+  }
+  else {
+     printf("you got it wrong");
+  }
+
+}
 
