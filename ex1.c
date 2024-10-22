e7c14b36235b7bfa009ed8d47dd8bfc132fca36a
MuhammedElubeyid <eprrmkz12345@gmail.com>
2024-10-22 22:21:28 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void) {
-int num = get_int("guess a number between 1 and 5");
-  if (num>=0 || num<= 5){
-   printf("you got it right");
-  }
-  else {
-     printf("you got it wrong");
-  }
+int main(void){
+  int secretNum= 3;
+int num= get_int("Guess a number between 1 and 5");
 
+  if(secretNum== num){
+    printf("you got it right");
+  }
+  else{
+    printf("you got it wrong");
+  }
 }
 
