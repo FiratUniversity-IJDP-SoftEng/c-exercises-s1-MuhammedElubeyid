0a9ead4e4d636582b515cbe328ce355657790036
MuhammedElubeyid <eprrmkz12345@gmail.com>
2024-10-22 22:59:29 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <cs50.h>
 
 int main(void){
   int secretNum= 3;
-int num= get_int("Guess a number between 1 and 5");
-
+  int num;
+  printf("Guess a number between 1 and 5\n");
+  scanf("%d",&num);
   if(secretNum== num){
     printf("you got it right");
   }
